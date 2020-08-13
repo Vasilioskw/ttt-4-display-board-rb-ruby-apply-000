a08d7e136481fdc4a8612dca8b303f89cc903533
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-13 16:59:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+put=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board("board","X","0")
+
+
+    end
